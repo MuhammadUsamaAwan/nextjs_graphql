3efac512d829393b6d4940b8169dc261a0107913
Muhammad Usama <underusama@gmail.com>
2023-07-28 19:23:00 +0500

    fixing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,31 @@ import { User, columns } from './columns'
 import { DataTable } from './data-table'
 
 async function getUsers(): Promise<User[]> {
-  const response = await fetch(`${process.env.NEXT_URL}/graphql`, {
-    method: 'POST',
-    body: JSON.stringify({
-      query: /* GraphQL */ `
-        {
-          users {
-            _id
-            username
-            email
-            phone
-            companyName
-            linkedinProfileUrl
-          }
-        }
-      `,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    next: {
-      revalidate: 10,
-    },
-  })
-  const data = await response.json()
-  return data.data.users
+  // const response = await fetch(`${process.env.NEXT_URL}/graphql`, {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     query: /* GraphQL */ `
+  //       {
+  //         users {
+  //           _id
+  //           username
+  //           email
+  //           phone
+  //           companyName
+  //           linkedinProfileUrl
+  //         }
+  //       }
+  //     `,
+  //   }),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   next: {
+  //     revalidate: 10,
+  //   },
+  // })
+  // const data = await response.json()
+  return []
 }
 
 export default async function HomePage() {
